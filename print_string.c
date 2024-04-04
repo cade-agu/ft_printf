e2713d088c9464bd3bf5458b6a808b216bb505e6
Carmen de Aguilar <153908439+cade-agu@users.noreply.github.com>
2024-04-04 17:25:52 +0200

    Update print_string.c

--- print_string.c
+++ print_string.c
@@ -1,1 +1,17 @@
+#include "ft_printf.h"
 
+int	ft_print_string(char *str)
+{
+	int	size;
+
+	size = 0;
+	if (str == 0)
+	{
+		return (ft_print_string("(null)"));
+	}
+	while (str[size])
+	{
+		size += ft_print_character(str[size]);
+	}
+	return (size);
+}
