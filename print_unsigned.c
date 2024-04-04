3bdab1f9dc59a4ff5764ec41628a0216fbf61a72
Carmen de Aguilar <153908439+cade-agu@users.noreply.github.com>
2024-04-04 17:27:20 +0200

    Update print_unsigned.c

--- print_unsigned.c
+++ print_unsigned.c
@@ -1,1 +1,22 @@
 
+#include "ft_printf.h"
+
+int ft_print_unsigned(unsigned int n)
+{
+	int	size;
+
+	size = 0;
+	if (n == 0)
+		size += ft_print_character('0');
+	else
+	{
+		if (n / 10 != 0)
+			ft_print_unsigned (n / 10);
+		ft_print_character ((n % 10) + '0');
+		while (n > 0)
+		{
+			n /= 10;
+			size++;
+		}
+	}
+	return (size);
