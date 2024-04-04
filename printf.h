e9c73db4a06cb63befa07c3623f36f8d3dade56b
Carmen de Aguilar <153908439+cade-agu@users.noreply.github.com>
2024-04-04 17:29:03 +0200

    Update printf.h

--- printf.h
+++ printf.h
@@ -1,1 +1,18 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_print_character(int character);
+int	ft_print_string(char *str);
+int	ft_print_number(int n);
+int	ft_print_unsigned(unsigned int n);
+int	ft_print_pointer(unsigned long long ptr);
+int	ft_print_hexadecimal(unsigned int num, const char word);
+
+#endif
