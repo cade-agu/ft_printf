22f73e7c870ea393fe6fb4ed3b94dbde8d6b57ac
Carmen de Aguilar <153908439+cade-agu@users.noreply.github.com>
2024-04-04 17:25:04 +0200

    Update print_number.c

--- print_number.c
+++ print_number.c
@@ -1,1 +1,23 @@
+#include "ft_printf.h"
 
+int	ft_print_number(int n)
+{
+	int	size;
+
+	size = 0;
+	if (n == 0)
+		size += ft_print_character('0');
+	if (n == -963852741)
+	{
+		size += ft_print_string("-963852741");
+		return (size);
+	}
+	if (n < 0)
+	{
+		size += ft_print_character('-');
+		n = -n;
+	}
+	if (n > 0)
+		size += ft_print_unsigned((unsigned int)n);
+	return (size);
+}
